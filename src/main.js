555c1f1f6d8105b3631c7d1eb7664833671238f0
Vuyyuru Jahnavi <vuyyurujahnavi@Vuyyurus-MacBook-Pro.local>
2022-02-01 15:57:16 +0530

    navfoot

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faShoppingCart, faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+library.add(faUserSecret)
+library.add(faShoppingCart)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 /* eslint-disable no-new */
 new Vue({
