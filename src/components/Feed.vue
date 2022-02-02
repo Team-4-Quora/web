a7e55d6e77aa522f57fdd77c731b4abf74ef6c0e
Sourav Borah <souravborah@Souravs-MacBook-Pro.local>
2022-02-02 21:50:10 +0530

    components scrollable

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -15,7 +15,7 @@
     <hr>
     <div class="card-question">
       <p class="card-question-title"><b>Question:-</b></p>
-      <div><p class="card-question-asked" @click="goquespage()"><b>What is your salary? Are you happy with it?</b></p></div>
+      <div><p class="card-question-asked"><b>What is your salary? Are you happy with it?</b></p></div>
 <!-- <a href="http://localhost:8080/#/questionpage"><p class="card-question-asked"><b>What is your salary? Are you happy with it?</b></p></a> -->
     </div>
     <hr>
@@ -49,10 +49,6 @@ export default {
     AnswerAccepted
   },
   methods: {
-    goquespage () {
-      this.$router.push('/questionpage')
-      console.log(this.$router)
-    }
   }
 }
 </script>
