7c8bce9f85e4d01caa11c5a2ff6662edadd21925
Sourav Borah <souravborah@Souravs-MacBook-Pro.local>
2022-02-02 19:07:40 +0530

    ads

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,7 @@
         <div><Feed/></div>
         </div>
         <div class="emptyDiv">
-            <QuestionPage/>
+            <Ads/>
         </div>
     </div>
      <div><Footer/></div>
@@ -22,6 +22,7 @@ import Footer from '@/components/Footer.vue'
 import Feed from '@/components/Feed.vue'
 import Sidebar from '@/components/Sidebar.vue'
 import QuestionPage from '@/components/QuestionPage.vue'
+import Ads from '@/components/Ads.vue'
 export default {
   name: 'Home',
   components: {
@@ -30,7 +31,8 @@ export default {
     Footer,
     Feed,
     Sidebar,
-    QuestionPage
+    QuestionPage,
+    Ads
   }
 }
 </script>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -14,12 +14,79 @@
             </div>
         </div>
         <hr>
-        <div class="follow-details">
-            <div class="following">
-
+        <h2 class="followers-text">Followers:-</h2>
+        <div class="container pt-4 text-center d-flex flex-nowrap overflow-auto scrollbar">
+            <div id="card-whole" class="col-12 col-md-2 ">
+                <div class="card shadow h-100 item">
+                  <img class="card-img-top img-fluid recomImg" src="@/assets/profile.png">
+                    <div class="card-body">
+                        <div class="card-title" ><h4>Name</h4></div>
+                    </div>
+                </div>
             </div>
-            <div class="followers">
-
+            <div id="card-whole" class="col-12 col-md-2 ">
+                <div class="card shadow h-100 item">
+                  <img class="card-img-top img-fluid recomImg" src="@/assets/profile.png">
+                    <div class="card-body">
+                        <div class="card-title" ><h4>Name</h4></div>
+                    </div>
+                </div>
+            </div>
+            <div id="card-whole" class="col-12 col-md-2 ">
+                <div class="card shadow h-100 item">
+                  <img class="card-img-top img-fluid recomImg" src="@/assets/profile.png">
+                    <div class="card-body">
+                        <div class="card-title" ><h4>Name</h4></div>
+                    </div>
+                </div>
+            </div>
+            <div id="card-whole" class="col-12 col-md-2 ">
+                <div class="card shadow h-100 item">
+                  <img class="card-img-top img-fluid recomImg" src="@/assets/profile.png">
+                    <div class="card-body">
+                        <div class="card-title" ><h4>Name</h4></div>
+                    </div>
+                </div>
+            </div>
+            <div id="card-whole" class="col-12 col-md-2 ">
+                <div class="card shadow h-100 item">
+                  <img class="card-img-top img-fluid recomImg" src="@/assets/profile.png">
+                    <div class="card-body">
+                        <div class="card-title" ><h4>Name</h4></div>
+                    </div>
+                </div>
+            </div>
+            <div id="card-whole" class="col-12 col-md-2 ">
+                <div class="card shadow h-100 item">
+                  <img class="card-img-top img-fluid recomImg" src="@/assets/profile.png">
+                    <div class="card-body">
+                        <div class="card-title" ><h4>Name</h4></div>
+                    </div>
+                </div>
+            </div>
+            <div id="card-whole" class="col-12 col-md-2 ">
+                <div class="card shadow h-100 item">
+                  <img class="card-img-top img-fluid recomImg" src="@/assets/profile.png">
+                    <div class="card-body">
+                        <div class="card-title" ><h4>Name</h4></div>
+                    </div>
+                </div>
+            </div>
+            <div id="card-whole" class="col-12 col-md-2 ">
+                <div class="card shadow h-100 item">
+                  <img class="card-img-top img-fluid recomImg" src="@/assets/profile.png">
+                    <div class="card-body">
+                        <div class="card-title" ><h4>Name</h4></div>
+                    </div>
+                </div>
+            </div>
+            <div id="card-whole" class="col-12 col-md-2 ">
+                <div class="card shadow h-100 item">
+                  <img class="card-img-top img-fluid recomImg" src="@/assets/profile.png">
+                    <div class="card-body">
+                        <div class="card-title" ><h4>Name</h4></div>
+                    </div>
+                </div>
             </div>
         </div>
         <div><Footer/></div>
@@ -55,5 +122,45 @@ export default {
 h4{
     font-family: Georgia, 'Times New Roman', Times, serif;
     font-weight: bold;
+}
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 120px;
+  margin-left: 50px;
+  margin-right: 50px;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+.card-image{
+    padding-left: 10px;
+}
+.card-text{
+    text-align: center;
+    font-family: cursive;
+    color: red;
+}
+.followers-text{
+    text-align: center;
+    font-family: fantasy;
+    color: blue;
+    font-weight: bold;
+}
+.follow-details{
+    display: flex;
+    justify-content: space-between;
+}
+.scrollbar::-webkit-scrollbar
+{
+    display: none;
+}
+#card-whole{
+    cursor: pointer;
 }
 </style>
