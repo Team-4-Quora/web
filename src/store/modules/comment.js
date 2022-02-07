dd2d4e72d2c87cbcc992590a902b893fd9fc6f8e
Vijaya Shukla <vijayashukla@Vijayas-MacBook-Pro.local>
2022-02-07 16:37:00 +0530

    edit

--- src/store/modules/comment.js
+++ src/store/modules/comment.js
@@ -14,11 +14,7 @@ const getters = {
 const actions = {
   async submitComment ({commit}, {answerId, message, commentBy, parentComponent}) {
     console.log('action started', answerId, message, commentBy, parentComponent)
-<<<<<<< HEAD
-    axios.post('http://10.177.1.74:8081/qna/comment/add', {
-=======
     axios.post('http://10.177.1.115:8081/qna/comment/add', {
->>>>>>> 98fbab17923ad3438a9d70659c995ab8ed554aff
       answerId: answerId,
       message: message,
       commentBy: commentBy,
--- src/store/modules/reaction.js
+++ src/store/modules/reaction.js
@@ -14,11 +14,7 @@ const actions = {
   // adding reaction to questions
   async addReaction ({commit}, {questionId, reactionBy, like}) {
     console.log('action started', questionId, reactionBy, like)
-<<<<<<< HEAD
-    axios.post('http://10.177.1.74:8081/qna/reaction/add', {
-=======
     axios.post('http://10.177.1.115:8081/qna/reaction/add', {
->>>>>>> 98fbab17923ad3438a9d70659c995ab8ed554aff
       questionId: questionId,
       reactionBy: reactionBy,
       like: like
@@ -32,11 +28,7 @@ const actions = {
   // adding reaction to answers
   async addReactionAns ({commit}, {answerId, reactionBy, like}) {
     console.log('action started', answerId, reactionBy, like)
-<<<<<<< HEAD
-    axios.post('http://10.177.1.74:8081/qna/reaction/add', {
-=======
     axios.post('http://10.177.1.115:8081/qna/reaction/add', {
->>>>>>> 98fbab17923ad3438a9d70659c995ab8ed554aff
       answerId: answerId,
       reactionBy: reactionBy,
       like: like
@@ -50,11 +42,7 @@ const actions = {
   // fetching all reactions on a question
   async getByQueReactions ({commit}, {id}) {
     console.log('fetch by email action', id)
-<<<<<<< HEAD
-    const response = await axios.get(`http://10.177.1.74:8081/qna/reaction/fetch/question/${id}`)
-=======
     const response = await axios.get(`http://10.177.1.115:8081/qna/reaction/fetch/question/${id}`)
->>>>>>> 98fbab17923ad3438a9d70659c995ab8ed554aff
     commit('setByQueReactions', response.data)
   }
 
